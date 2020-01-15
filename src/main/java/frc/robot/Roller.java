55a4ab82e6d0dc803f3a3967f05c4c0c747eaf6f
Michaelteklegh <tekleghiorghismichael@gmail.com>
2020-01-15 17:19:44 -0600

    Added Roller class

---
+++ src/main/java/frc/robot/Roller.java
@@ -0,0 +1,41 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Roller implements IRoller
+{
+    private TalonSRX rollerMotor;
+
+    private double motorSpeed;
+    private static final double HIGH = 0.9;
+    private static final double LOW = 0.0;
+
+    public Roller() {
+        rollerMotor = new TalonSRX(PortMap.CAN.ROLLER_MOTOR_CONTROLLER);
+        //rollerMotor.setInverted(true);
+
+        stop();
+    }
+
+    @Override
+    public void stop() {
+        motorSpeed = LOW;
+    }
+
+    @Override
+    public void start() {
+        motorSpeed = HIGH;
+        
+    }
+
+    @Override
+    public void init() {
+        stop();
+    }
+
+    @Override
+    public void periodic() {
+        rollerMotor.set(ControlMode.PercentOutput,motorSpeed);
+    }
+}
