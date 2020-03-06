055e102a87ed4909731aac5615a45140f5d195d1
jwoods7097 <jwoods7097@gmail.com>
2020-03-06 12:54:58 -0600

    Revised endgame and revised and condensed launcher

---
+++ src/main/java/frc/robot/ILauncher.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+public interface ILauncher {
+
+    public void init();
+
+    public void intake();
+
+    public void advance();
+
+    public void reverse();
+
+    public void shoot();
+
+    public void periodic();
+
+}
--- src/main/java/frc/robot/StorageMode.java
+++
@@ -1,6 +0,0 @@
-package frc.robot;
-
-public enum StorageMode {
-    COLLECTING,
-    FIRING
-}
