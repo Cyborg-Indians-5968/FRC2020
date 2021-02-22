84a012070f139eef756f51a610ce9a63b731abbf
innyun <vinnysun1@gmail.com>
2021-02-22 16:13:03 -0600

    changed AutoNav to absolute direction

--- src/main/java/frc/robot/AutoNavBounce.java
+++ src/main/java/frc/robot/AutoNavBounce.java
@@ -15,35 +15,35 @@ public class AutoNavBounce implements IRobotMode {
     }
 
     private void driveStart() { // Step 1
-        drive.driveDistance(60, speed, -45, () -> driveStraightLong());
+        drive.driveDistance(60, speed, 45, () -> driveStraightLong());
     }
     
     public void driveStraightLong() { // Step 2
-        drive.driveDistance(120, speed, 18, () -> driveStraightShort());
+        drive.driveDistance(120, speed, 162, () -> driveStraightShort());
     }
 
     public void driveStraightShort() { // Step 3
-        drive.driveDistance(12, speed, 0, () -> driveStraightLong1());
+        drive.driveDistance(12, speed, 90, () -> driveStraightLong1());
     }
 
     public void driveStraightLong1() { // Step 4
-        drive.driveDistance(106, speed, 5, () -> driveStraightLong2());
+        drive.driveDistance(106, speed, 0, () -> driveStraightLong2());
     }
 
     public void driveStraightLong2() { // Step 5
-        drive.driveDistance(106, speed, -5, () -> driveStraightShort1());
+        drive.driveDistance(106, speed, 180, () -> driveStraightShort1());
     }
 
     public void driveStraightShort1() { // Step 6
-        drive.driveDistance(40, speed, 5, () -> driveStraightShort3());
+        drive.driveDistance(40, speed, 90, () -> driveStraightShort3());
     }
 
     public void driveStraightShort3() { // Step 7
-        drive.driveDistance(67, speed, 45, () -> driveStraightEnd());
+        drive.driveDistance(67, speed, 10, () -> driveStraightEnd());
     }
 
     public void driveStraightEnd() { // Step 8
-        drive.driveDistance(35, speed, 0, null);
+        drive.driveDistance(35, speed, 135, null);
     }
 
     @Override
